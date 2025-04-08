fb9f21a995111c4d9791c60eb5bb793e7b71a567
LegendaryPloxerd <mimojih@gmail.com>
2025-04-09 00:33:01 +0200

    PASSWORD GENERATOR DONE!

--- Web/generator/interact.js
+++ Web/generator/interact.js
@@ -1,33 +1,69 @@
-const symBtn = document.getElementById('symbols');
-const numBtn = document.getElementById('numbers');
-const upperBtn = document.getElementById('uppercase');
-const lowerBtn = document.getElementById('lowercase');
+const symBtn = document.getElementById('sym');
+const numBtn = document.getElementById('num');
+const upperBtn = document.getElementById('upper');
+const lowerBtn = document.getElementById('lower');
 const genBtn = document.getElementById('generate');
 const slider = document.getElementById('range');
 const display = document.getElementById('screen');
 let password_length = document.getElementById("myPassLen");
   
-let types = [
-    "123456789",
-    "abcdefghijklmnopqrstuvwxyz", 
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZ", 
-    "!@#£¤$%&/{[()]}=+?`¨^~*'-_.:,;<>|"
-];
+let types = {
+    num: "123456789",
+    lower: "abcdefghijklmnopqrstuvwxyz",
+    upper: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    sym: "!@#£¤$%&/{[()]}=+?`¨^~*'-_.:,;<>|"
+};
+
+let states = {
+    isSym: false,
+    isNum: false,
+    isLower: false,
+    isUpper: false
+};
 
 function Generation() {
-    let storedPass = []
-    for (let i = 0; i < parseInt(password_length.textContent); i++) { 
-        storedPass.push(types[1][Math.floor(Math.random() * 5)])
+
+    let chars = "";
+    if (states.isLower) chars += types.lower;
+    if (states.isUpper) chars += types.upper;
+    if (states.isNum) chars += types.num;
+    if (states.isSym) chars += types.sym;
+    if (chars === "") return display.textContent = "Select at least one type";
+
+    let password = "";
+    for (let i = 0; i < parseInt(password_length.textContent); i++) {
+        password += chars[Math.floor(Math.random() * chars.length)];
     }
-    return display.textContent = storedPass.slice(0,storedPass.length).join('')
+
+    return display.textContent = password;
+};
+
+function stateHandling(button,key) {
+    button.addEventListener("click", () => {
+        states[key] = !states[key]; 
+        button.style.opacity = states[key] ? "60%" : "100%";
+        button.textContent = button.textContent.includes("Add") ? button.textContent.replace("Add","Remove") : button.textContent.replace("Remove","Add")
+    })
+}
+
+function buttonHandling() {
+    stateHandling(symBtn, "isSym");
+    stateHandling(numBtn, "isNum")
+    stateHandling(lowerBtn, "isLower")
+    stateHandling(upperBtn, "isUpper")
 }
 
 function GeneratePassword() {
     genBtn.addEventListener("click",() => {
-        Generation()
+        Generation();
     })
     slider.addEventListener("input", () => {
         password_length.textContent = slider.value;
     })
 }
-GeneratePassword()
+
+function init() {
+    GeneratePassword();
+    buttonHandling();
+}
+init();
